63f32f7182f42806380297680bf585b2e669e04c
ubuntu <quentinrougemont@orange.fr>
2018-04-05 03:46:12 -0400

    reshaped scripts

--- 00-scripts/03.sam_pileup.sh
+++ 00-scripts/03.sam_pileup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-list_bam="list_pools"
 genome="06-genome/your_genome.fasta"
 out="05-popoolation"
 samtools mpileup -f $genome -B $(ls ./04-all_samples/*sort.bam) -d 10000  > $out/pool.mpileup
--- 00-scripts/04.pyleup_synchro.sh
+++ 00-scripts/04.pyleup_synchro.sh
@@ -6,8 +6,9 @@ folder="05-popoolation"
 in="--input"
 out="--output"
 fq="--fastq-type sanger"
-qual="--min-qual 30"
-th="--threads 8" 
 
-java -ea -Xmx7g -jar "$path"/mpileup2sync.jar "$in" "$folder"/pool.mpileup $out $folder/pool.sync  --min-qual 20 --threads 8
+java -ea -Xmx7g -jar "$path"/mpileup2sync.jar "$in" "$folder"/pool.mpileup \
+    $out $folder/pool.sync  \
+    --min-qual 20 \
+    --threads 8
 echo "job pool2sync.jar  successfuly finished"
